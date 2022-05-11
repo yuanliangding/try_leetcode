b2e1f976864cf7ff1afd472a155b9a2d1122d920
yuanliangding <yuanliangding@126.com>
2022-05-11 16:05:02 +0800

    leetcode 第三题

---
+++ python/leetcode_00003.py
@@ -0,0 +1,34 @@
+# **********
+# 给定一个字符串 s ，请你找出其中不含有重复字符的 最长子串 的长度。
+#
+# 示例 1:
+# 输入: s = "abcabcbb"
+# 输出: 3
+# 解释: 因为无重复字符的最长子串是 "abc"，所以其长度为 3。
+#
+# 示例 2:
+# 输入: s = "bbbbb"
+# 输出: 1
+# 解释: 因为无重复字符的最长子串是 "b"，所以其长度为 1。
+#
+# 示例 3:
+# 输入: s = "pwwkew"
+# 输出: 3
+# 解释: 因为无重复字符的最长子串是 "wke"，所以其长度为 3。
+#     请注意，你的答案必须是 子串 的长度，"pwke" 是一个子序列，不是子串。
+#
+# 提示：
+# 0 <= s.length <= 5 * 104
+# s 由英文字母、数字、符号和空格组成
+# **********
+
+def length_of_longest_substring(s):
+    """
+    :type s: str
+    :rtype: int
+    """
+
+
+assert length_of_longest_substring("abcabcbb") == 3
+assert length_of_longest_substring("bbbbb") == 1
+assert length_of_longest_substring("pwwkew") == 3
